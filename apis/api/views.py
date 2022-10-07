e24e5b72ba9c7e1550cacadcd4cac0a33c3216de
Haseeb Shaikh <haseeb.shaikh@ggcollege.in>
2022-10-07 11:46:36 +0000

    done

--- apis/api/views.py
+++ apis/api/views.py
@@ -6,9 +6,9 @@ from .form import GeeksForm
 
 # Create your views here.
 def test_api(request):
-    subscribers = ApiModel.objects.values('title', 'description')
+    movies = ApiModel.objects.values('title', 'description')
     data = {
-        'subscribers' : list(subscribers)
+        'Movies' : list(movies)
     }
     return JsonResponse(data)
   
@@ -16,11 +16,11 @@ def create_view(request):
     # dictionary for initial data with 
     # field names as keys
     context ={}
-  
-    # add the dictionary during initialization
+
+        
+        # add the dictionary during initialization
     form = GeeksForm(request.POST or None)
     if form.is_valid():
         form.save()
-          
     context['form']= form
     return render(request, "create_view.html", context)
